b0d86195986689257bb2b4905b77d6080272971a
AqingCyan <aqingcyan@gmail.com>
2022-12-20 04:10:49 +0800

    feat: 支持热更新

--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -5,12 +5,27 @@ import { DEFAULT_TEMPLATE_PATH } from '../constants'
 export function pluginIndexHtml(): Plugin {
   return {
     name: 'island:index-html',
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: '/src/runtime/client-entry.tsx',
+            },
+            injectTo: 'body',
+          },
+        ],
+      }
+    },
     configureServer(server) {
       return () => {
-        server.middlewares.use(async (req, res, next) => {
-          // 1.读取 template.html 的内容
-          const content = await readFile(DEFAULT_TEMPLATE_PATH)
-          // 2.响应 HTML
+        server.middlewares.use(async (req, res) => {
+          // 读取 template.html 的内容
+          let content = await readFile(DEFAULT_TEMPLATE_PATH, 'utf-8')
+          content = await server.transformIndexHtml(req.url, content, req.originalUrl)
           res.setHeader('Content-Type', 'text/html')
           res.end(content)
         })
