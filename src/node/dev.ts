f70468bc6cb2746652c663a0ceec4bf4adb13315
AqingCyan <aqingcyan@gmail.com>
2022-12-27 09:22:22 +0800

    feat: 自动读取目录结构生成路由插件（约定式路由）

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -4,6 +4,7 @@ import pluginReact from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
 import { resolveConfig } from './config';
 import { pluginConfig } from './plugin-island/config';
+import { pluginRoutes } from './plugin-routes';
 
 export async function createDevServer(
   root: string,
@@ -12,7 +13,12 @@ export async function createDevServer(
   const config = await resolveConfig(root, 'serve', 'development');
   return createServer({
     root: PACKAGE_ROOT,
-    plugins: [pluginIndexHtml(), pluginReact(), pluginConfig(config, restart)],
+    plugins: [
+      pluginIndexHtml(),
+      pluginReact(),
+      pluginConfig(config, restart),
+      pluginRoutes({ root: config.root })
+    ],
     server: { fs: { allow: [PACKAGE_ROOT] } }
   });
 }
